b14f68f17a111c6ab47dada7b54c11a5709f6375
Nicola DiPasquale <nicholas.dipasquale@gmail.com>
2013-09-06 22:58:14 -0400

    Fixed color scheme

--- interval.go
+++ interval.go
@@ -7,6 +7,14 @@ type Interval struct {
         Start, End, Step float64
 }
 
+// Make an Interval instance with the provided start and end taking
+// the specified number of steps (this will calculate the value for
+// step)
+func LinearSpacing(start, end float64, numSteps int) Interval {
+  step := (end - start) / float64(numSteps)
+  return Interval{ start, end, step }
+}
+
 // Range is a method on Interval that returns a channel that is
 // populated by the values within the interval.
 func (i Interval) Range() <-chan float64 {
--- mandelbrot.go
+++ mandelbrot.go
@@ -2,39 +2,46 @@ package main
 
 import (
 	"fmt"
-	// "image"
+	"image"
 	"image/color"
-	// "image/png"
 	"math"
 	"math/cmplx"
-	// "os"
 )
 
-func HSVToRGB(h, s, v float64) (uint8, uint8, uint8) {
-  r, g, b := v, v, v
-  if (s != 0.0) {
-    h = math.Mod(h * 6.0, 6.0)
-    s = s * v
-    v = s * (1.0 - math.Abs(math.Mod(h, 2.0) - 1.0))
-
-    f := [][]float64{
-      { s, v, 0.0 },
-      { v, s, 0.0 },
-      { 0.0, s, v },
-      { 0.0, v, s },
-      { v, 0.0, s },
-      { s, 0.0, v },
-    }
-
-    r, g, b = f[int32(math.Floor(h))][0], f[int32(math.Floor(h))][1], f[int32(math.Floor(h))][2]
-  }
-
-  return uint8(r * 255.0), uint8(g * 255.0), uint8(b * 255.0)
+type Mandelbrot struct {
+  X, Y, Zoom float64
+  Iterations int
 }
 
-func getColor(i, s int32) color.Color {
-  r, g, b := HSVToRGB(math.Mod(1.0 - (float64(i) / float64(s) + 0.3333), 1.0), 1.0, 1.0)
-  return color.RGBA{ r, g, b, 1.0 }
+func (m Mandelbrot) colorAt(i int) color.Color {
+  r, g, b := HSVToRGB(1.0 - math.Mod(float64(i) / float64(m.Iterations) + 0.3333, 1.0), 1.0, 1.0)
+  return color.RGBA{ r, g, b, 255 }
+}
+
+func (m Mandelbrot) calculate(c complex128) color.Color {
+  limit := 2.0 * m.Zoom
+  z := complex(0.0, 0.0)
+  for i := 0; i < m.Iterations; i++ {
+    z = z * z + c
+    if cmplx.Abs(z) > limit {
+      return m.colorAt(i)
+    }
+  }
+
+  return color.Black
+}
+
+func (m Mandelbrot) Plot(img *image.RGBA) {
+  bounds := img.Bounds()
+
+  ix, iy := LinearSpacing(m.X - m.Zoom, m.X + m.Zoom, bounds.Dx()),
+    LinearSpacing(m.Y - m.Zoom, m.Y + m.Zoom, bounds.Dy())
+
+  for y, j := bounds.Min.Y, iy.Start; y < bounds.Max.Y; y, j = y + 1, j + iy.Step {
+    for x, i := bounds.Min.X, ix.Start; x < bounds.Max.X; x, i = x + 1, i + ix.Step {
+      img.Set(x, y, m.calculate(complex(i, j)))
+    }
+  }
 }
 
 func mandelbrot(c complex128) string {
@@ -66,4 +73,13 @@ func main() {
 	s := plot(Interval{-2.05, 1.05, 0.03}, Interval{-1.2, 1.2, 0.05})
 
 	fmt.Printf("%s", s)
+
+  m := Mandelbrot{ -0.5, 0.0, 2.0, 256 }
+
+  img := image.NewRGBA(image.Rect(0, 0, 1440, 900))
+
+  m.Plot(img)
+
+  WritePNG(img, "output.png")
 }
+
